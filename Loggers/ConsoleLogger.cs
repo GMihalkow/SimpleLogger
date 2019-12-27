df464f8e565a507b66bdc733da028f8fd5e40d13
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-12-27 16:31:12 +0200

    Added the console and the email loggers.

---
+++ Loggers/ConsoleLogger.cs
@@ -0,0 +1,21 @@
+﻿using SimpleLogger.Models.Console.Configuration;
+using SimpleLogger.Models.Contracts;
+using System;
+
+namespace SimpleLogger.Loggers
+{
+    public sealed class ConsoleLogger : ICustomLogger
+    {
+        internal ConsoleLogger(ConsoleLoggerConfig config)
+        {
+            this.Color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), config.TextColor.ToString());
+        }
+
+        internal ConsoleColor Color { get; private set; }
+
+        public void Log(System.Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Loggers/EmailLogger.cs
@@ -0,0 +1,56 @@
+﻿using SimpleLogger.Models.Contracts;
+using SimpleLogger.Models.Email.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+namespace SimpleLogger.Loggers
+{
+    internal sealed class EmailLogger : ICustomLogger
+    {
+        private readonly EmailLoggerConfig config;
+
+        public EmailLogger(EmailLoggerConfig config)
+        {
+            this.config = config;
+        }
+
+        public void Log(System.Exception ex)
+        {
+            var template = @"Date: {{date}}\n" +
+                            "Thread: {{thread}}\n" +
+                            "Application: {{app}}\n" +
+                            "Exception Type: {{exceptionType}}\n" +
+                            "Exception Message: {{exceptionMessage}}\n" +
+                            "Stack Trace: {{stackTrace}}\n" +
+                            "Log Level: {{level}}\n" +
+                            "Inner Exception Type: {{innerExceptionType}}\n" +
+                            "Inner Exception Message: {{innerExceptionMessage}}\n" +
+                            "Inner Exception Stack Trace: {{innerExceptionStackTrace}}";
+
+            template = template.Replace("{{date}}", DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            template = template.Replace("{{thread}}", Thread.CurrentThread.ManagedThreadId.ToString());
+            template = template.Replace("{{app}}", this.config.ApplicationName);
+            template = template.Replace("{{exceptionType}}", ex.GetType().FullName);
+            template = template.Replace("{{exceptionMessage}}", ex.Message);
+            template = template.Replace("{{stackTrace}}", ex.StackTrace);
+            template = template.Replace("{{level}}", this.config.Level.ToString());
+            template = template.Replace("{{innerExceptionType}}", ex.InnerException?.GetType().Name ?? "None");
+            template = template.Replace("{{innerExceptionMessage}}", ex.InnerException?.Message ?? "None");
+            template = template.Replace("{{innerExceptionStackTrace}}", ex.InnerException?.StackTrace ?? "None");
+
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                Host = this.config.SmtpHost,
+                Port = this.config.SmtpPort,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(this.config.Username, this.config.Password),
+            };
+            
+            smtpClient.Send(new MailMessage(this.config.From, this.config.To, this.config.ApplicationName, template));
+        }
+    }
+}
