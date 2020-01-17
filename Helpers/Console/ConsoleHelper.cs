b7698c96fa9240dece3a6e3429fc7968fb03d4e5
Georgimihalkov <Georgimihalkov@abv.bg>
2020-01-17 22:57:40 +0000

    Added a Console helper class.

---
+++ Helpers/Console/ConsoleHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleLogger.Helpers
+{
+    internal class ConsoleHelper
+    {
+        internal static void Log(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+    }
+}
